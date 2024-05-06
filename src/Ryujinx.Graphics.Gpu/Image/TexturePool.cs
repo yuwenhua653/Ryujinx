a69ca1de2f351ba0518fc6a3e14c5f90ecd27c09
fuwenshi <zhoudecheng456@outlook.com>
2024-05-06 10:18:53 +0800

    Add files via upload

--- src/Ryujinx.Graphics.Gpu/Image/TexturePool.cs
+++ src/Ryujinx.Graphics.Gpu/Image/TexturePool.cs
@@ -484,7 +484,7 @@ namespace Ryujinx.Graphics.Gpu.Image
                         depthOrLayers = Math.Max(1, depthOrLayers >> minLod);
                     }
 
-                    (gobBlocksInY, gobBlocksInZ) = SizeCalculator.GetMipGobBlockSizes(height, depth, formatInfo.BlockHeight, gobBlocksInY, gobBlocksInZ, minLod);
+                    (gobBlocksInY, gobBlocksInZ) = SizeCalculator.GetMipGobBlockSizes(height, depth, formatInfo.BlockHeight, gobBlocksInY, gobBlocksInZ);
                 }
 
                 levels = (maxLod - minLod) + 1;
